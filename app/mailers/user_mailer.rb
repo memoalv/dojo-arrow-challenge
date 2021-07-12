e1bbe1903835e421acc1aa8fdf2b0f552c14bd15
Guillermo Alvarez <guillermo@icalialabs.com>
2021-07-12 16:46:00 -0600

    Sends  the updated_account_email async

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,11 @@
 class UserMailer < ApplicationMailer
   def welcome_email
     @user = params[:user]
-    mail(to: @user.email, subject: 'Account registered correctly')
+    mail(to: @user.email, subject: 'Account registered correctly').deliver_later
   end
 
   def updated_account_email
     @user = params[:user]
-    mail(to: @user.email, subject: 'Account updated correctly')
+    mail(to: @user.email, subject: 'Account updated correctly').deliver_later
   end
 end

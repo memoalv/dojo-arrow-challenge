46bf00eae5f4f81db3f10e28323611943770b456
Guillermo Alvarez <guillermo@icalialabs.com>
2021-07-13 12:07:38 -0600

    Removes deliver_later method call

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,11 @@
 class UserMailer < ApplicationMailer
   def welcome_email
     @user = params[:user]
-    mail(to: @user.email, subject: 'Account registered correctly').deliver_later
+    mail(to: @user.email, subject: 'Account registered correctly')
   end
 
   def updated_account_email
     @user = params[:user]
-    mail(to: @user.email, subject: 'Account updated correctly').deliver_later
+    mail(to: @user.email, subject: 'Account updated correctly')
   end
 end
